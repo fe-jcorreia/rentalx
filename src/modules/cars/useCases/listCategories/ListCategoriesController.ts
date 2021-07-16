95076326f41e70a310da372ba58516baf89483ad
fernando <fe.jcorreia@gmail.com>
2021-07-16 19:29:15 -0300

    controller and useCase to list categories created

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoryUseCase: ListCategoriesUseCase) {}
+
+  handle(request: Request, response: Response) {
+    const allCategories = this.listCategoryUseCase.execute();
+    return response.status(200).json(allCategories);
+  }
+}
+
+export { ListCategoriesController };
