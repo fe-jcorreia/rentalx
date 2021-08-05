b53ae676669a51ec8cebd3c3777f275b1ec6984e
fernando <fe.jcorreia@gmail.com>
2021-08-05 13:36:37 -0300

    listCars by brand, category and name

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -11,7 +11,34 @@ describe("List Cars", () => {
   });
 
   it("should be able to list all available cars", async () => {
-    const cars = await listCarsUseCase.execute();
-    console.log(cars);
+    const car = await carsRepositoryInMemory.create({
+      name: "Car1",
+      description: "Car1 desc",
+      daily_rate: 140,
+      license_plate: "CAR1-1080",
+      fine_amount: 100,
+      brand: "Brand",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by brand", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car2",
+      description: "Car2 desc",
+      daily_rate: 140,
+      license_plate: "CAR2-1080",
+      fine_amount: 100,
+      brand: "Brand2",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({ brand: "Brand2" });
+
+    expect(cars).toEqual([car]);
   });
 });
