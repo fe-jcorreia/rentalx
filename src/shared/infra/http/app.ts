d0a9a4ff5ee547bd63506466f75f84b5b586f30e
fe-jcorreia <fe.jcorreia@gmail.com>
2022-01-31 16:57:44 -0300

    integration tests

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import { errorTreatment } from "@shared/infra/http/middlewares/errorTreatment";
+import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
+
+import swaggerFile from "../../../swagger.json";
+
+import "@shared/container";
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
+app.use(errorTreatment);
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,20 +1,3 @@
-import "reflect-metadata";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerFile from "swagger.json";
-
-import { errorTreatment } from "@shared/infra/http/middlewares/errorTreatment";
-import { router } from "@shared/infra/http/routes";
-import createConnection from "@shared/infra/typeorm";
-
-import "@shared/container";
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router);
-app.use(errorTreatment);
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is Running!"));
